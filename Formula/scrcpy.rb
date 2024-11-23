89dcbeb05f43102ec5faef0ba71989db992d0ab9
Karl Baumgartner <karl@limbar.io>
2024-11-23 11:54:23 +0300

    scrcpy: build linux-amd64 and darwin-arm64 static libraries and commit them to the repo
    Signed-off-by: Karl Baumgartner <karl@limbar.io>

--- Formula/scrcpy.rb
+++ Formula/scrcpy.rb
@@ -1,89 +1,43 @@
 class Scrcpy < Formula
   desc "Display and control your Android device"
   homepage "https://github.com/Genymobile/scrcpy"
-  url "https://github.com/Genymobile/scrcpy/archive/refs/tags/v2.7.tar.gz"
-  sha256 "3ceea215f6eccb59535f68a16db6db2b05a8a1c91bdcb4a6e222d3093a9daf8c"
+  version "v0.8.4"
   license "Apache-2.0"
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  depends_on "limbario/tap/adb"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/scrcpy-darwin-arm64"
+      sha256 "a9b5637bb20999bb580347ff12f3762a1f05b669505241ac4f179abd5078c15d" # replace_with_darwin_arm64_sha256
+    else
+      odie "This formula is not compatible with darwin-amd64. Please use `brew install scrcpy`"
+    end
   end
 
-  bottle do
-    sha256 arm64_sequoia: "f0fee1075318ee78ec35066cff51f5faef4d3af0c81ecdad25686f89394f3aa1"
-    sha256 arm64_sonoma:  "afaf0bf5fc3c48997ca8a36c626e40c673564c638ac951c44d667fc8782e1906"
-    sha256 arm64_ventura: "9ace7a93c8b40a371863463945b703f68424d78433803028bcd9a3b77f619920"
-    sha256 sonoma:        "f3b446582e45ca9972c8de85734ba16d293916479ae73e4f11a2884ef554df50"
-    sha256 ventura:       "84791d738ac1d933573c9ab27f773ab5b378257b4465122f8f1413caaa9d7545"
-    sha256 x86_64_linux:  "694bc1df5e0064f05df84169b5e07f7bbd1f54191d8d35892fc61eb771676d8e"
-  end
-
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
-  depends_on "pkgconf" => :build
-  # ffmpeg has too many dependencies so we use ffmpeg
-  depends_on "limbario/tap/ffmpeg"
-  depends_on "libusb"
-  depends_on "sdl2"
-
-  resource "prebuilt-server" do
-    url "https://github.com/Genymobile/scrcpy/releases/download/v2.7/scrcpy-server-v2.7", using: :nounzip
-    sha256 "a23c5659f36c260f105c022d27bcb3eafffa26070e7baa9eda66d01377a1adba"
+  on_linux do
+    if Hardware::CPU.arm?
+      odie "This formula is not compatible with linux-arm64. Please use `brew install scrcpy`"
+    else
+      url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/scrcpy-linux-amd64"
+      sha256 "0f21cb13dc7cfc7423ad706e39fcd10e8baba0eed249bc2f59c3605e0b1b3e18" # replace_with_linux_amd64_sha256
+    end
   end
 
   def install
-    odie "prebuilt-server resource needs to be updated" if version != resource("prebuilt-server").version
+    binary_name = "scrcpy"
+    binary_path = "scrcpy"
 
-    buildpath.install resource("prebuilt-server")
-    cp "scrcpy-server-v#{version}", "prebuilt-server.jar"
+    if OS.mac?
+      binary_path = Hardware::CPU.arm? ? "scrcpy-darwin-arm64" : "scrcpy-darwin-amd64"
+    elsif OS.linux?
+      binary_path = Hardware::CPU.arm? ? "scrcpy-linux-arm64" : "scrcpy-linux-amd64"
+    end
 
-    system "meson", "setup", "build", "-Dprebuilt_server=#{buildpath}/prebuilt-server.jar",
-                                      *std_meson_args
-    system "meson", "compile", "-C", "build", "--verbose"
-    system "meson", "install", "-C", "build"
+    bin.install binary_path => binary_name
   end
 
   test do
-    fakeadb = (testpath/"fakeadb.sh")
-
-    # When running, scrcpy calls adb five times:
-    #  - adb start-server
-    #  - adb devices -l
-    #  - adb -s SERIAL push ... (to push scrcpy-server.jar)
-    #  - adb -s SERIAL reverse ... tcp:PORT ...
-    #  - adb -s SERIAL shell ...
-    # However, exiting on $3 = shell didn't work properly, so instead
-    # fakeadb exits on $3 = reverse
-
-    fakeadb.write <<~EOS
-      #!/bin/sh
-      echo "$@" >> #{testpath/"fakeadb.log"}
-
-      if [ "$1" = "devices" ]; then
-        echo "List of devices attached"
-        echo "emulator-1337          device product:sdk_gphone64_x86_64 model:sdk_gphone64_x86_64 device:emulator64_x86_64_arm64 transport_id:1"
-      fi
-
-      if [ "$3" = "reverse" ]; then
-        exit 42
-      fi
-    EOS
-
-    fakeadb.chmod 0755
-    ENV["ADB"] = fakeadb
-
-    # It's expected to fail after adb reverse step because fakeadb exits
-    # with code 42
-    out = shell_output("#{bin}/scrcpy --no-display --record=file.mp4 -p 1337 2>&1", 1)
-    assert_match(/ 42/, out)
-
-    log_content = File.read(testpath/"fakeadb.log")
-
-    # Check that it used port we've specified
-    assert_match(/tcp:1337/, log_content)
-
-    # Check that it tried to push something from its prefix
-    assert_match(/push #{prefix}/, log_content)
+    system "#{bin}/scrcpy", "--version"
   end
 end
---
+++ scripts/scrcpy-linux-amd64.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <scrcpy version> <output_dir>"
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "Usage: $0 <scrcpy version> <output_dir>"
+    exit 1
+fi
+
+VERSION="$1"
+DIST_DIR="$2"
+
+# Configuration
+PREBUILT_SCRCPY_SERVER_VERSION="${VERSION}"
+PREBUILT_SCRCPY_SERVER_PATH="prebuilt/scrcpy-server-v${PREBUILT_SCRCPY_SERVER_VERSION}"
+FFMPEG_VERSION="7.1"
+SDL_VERSION="2.30.9"
+LIBUSB_VERSION="1.0.27"
+BUILD_DIR="build-linux-static"
+DEPS_DIR="deps-static"
+
+# Create directories
+mkdir -p "$DEPS_DIR"
+mkdir -p "$BUILD_DIR"
+
+download_prebuilt_server() {
+    echo "Downloading prebuilt server..."
+    mkdir -p prebuilt
+    curl -L "https://github.com/Genymobile/scrcpy/releases/download/v${PREBUILT_SCRCPY_SERVER_VERSION}/scrcpy-server-v${PREBUILT_SCRCPY_SERVER_VERSION}" \
+        -o "${PREBUILT_SCRCPY_SERVER_PATH}"
+}
+
+# Build libusb statically
+build_libusb() {
+    echo "Building libusb statically..."
+    cd "$DEPS_DIR"
+
+    if [ ! -f "libusb-$LIBUSB_VERSION.tar.bz2" ]; then
+        curl -LO "https://github.com/libusb/libusb/releases/download/v$LIBUSB_VERSION/libusb-$LIBUSB_VERSION.tar.bz2"
+    fi
+
+    tar xf "libusb-$LIBUSB_VERSION.tar.bz2"
+    cd "libusb-$LIBUSB_VERSION"
+
+    ./configure \
+        --prefix="$PWD/../../$DEPS_DIR/libusb-install" \
+        --enable-static \
+        --disable-shared \
+        --disable-udev
+
+    make -j$(nproc)
+    make install
+    cd ../..
+}
+
+# Build FFmpeg statically
+build_ffmpeg() {
+    echo "Building FFmpeg statically..."
+    cd "$DEPS_DIR"
+
+    if [ ! -f "ffmpeg-$FFMPEG_VERSION.tar.xz" ]; then
+        curl -LO "https://ffmpeg.org/releases/ffmpeg-$FFMPEG_VERSION.tar.xz"
+    fi
+
+    tar xf "ffmpeg-$FFMPEG_VERSION.tar.xz"
+    cd "ffmpeg-$FFMPEG_VERSION"
+
+    ./configure \
+        --prefix="$PWD/../../$DEPS_DIR/ffmpeg-install" \
+        --enable-static \
+        --disable-shared \
+        --disable-programs \
+        --disable-doc \
+        --disable-everything \
+        --enable-decoder=h264 \
+        --enable-decoder=hevc \
+        --enable-decoder=av1 \
+        --enable-decoder=pcm_s16le \
+        --enable-decoder=opus \
+        --enable-decoder=aac \
+        --enable-decoder=flac \
+        --enable-decoder=png \
+        --enable-protocol=file \
+        --enable-demuxer=image2 \
+        --enable-parser=png \
+        --enable-muxer=matroska \
+        --enable-muxer=mp4 \
+        --enable-muxer=opus \
+        --enable-muxer=flac \
+        --enable-muxer=wav \
+        --disable-vulkan \
+        --enable-pic \
+        --enable-swresample \
+        --pkg-config-flags="--static"
+
+    make -j$(nproc)
+    make install
+    cd ../..
+}
+
+# Build SDL2 statically
+build_sdl() {
+    echo "Building SDL2 statically..."
+    cd "$DEPS_DIR"
+    curl -LO "https://github.com/libsdl-org/SDL/releases/download/release-$SDL_VERSION/SDL2-$SDL_VERSION.tar.gz"
+    tar xf "SDL2-$SDL_VERSION.tar.gz"
+    cd "SDL2-$SDL_VERSION"
+
+    ./configure \
+        --prefix="$PWD/../../$DEPS_DIR/sdl-install" \
+        --enable-static \
+        --disable-shared \
+        --enable-video-x11 \
+        --enable-video-wayland
+
+    make -j$(nproc)
+    make install
+    cd ../..
+}
+
+download_prebuilt_server
+# Build static dependencies
+build_libusb
+build_ffmpeg
+build_sdl
+
+# Configure scrcpy with static dependencies
+PKG_CONFIG_PATH="$PWD/$DEPS_DIR/ffmpeg-install/lib/pkgconfig:$PWD/$DEPS_DIR/sdl-install/lib/pkgconfig:$PWD/$DEPS_DIR/libusb-install/lib/pkgconfig" \
+CFLAGS="-I$PWD/$DEPS_DIR/libusb-install/include -I$PWD/$DEPS_DIR/libusb-install/include/libusb-1.0" \
+CPPFLAGS="-I$PWD/$DEPS_DIR/libusb-install/include" \
+LDFLAGS="-L$PWD/$DEPS_DIR/libusb-install/lib -static-libgcc -static-libstdc++" \
+meson setup "$BUILD_DIR" \
+    --buildtype=release \
+    --strip \
+    -Db_staticpic=true \
+    -Db_lto=true \
+    -Dportable=true \
+    -Dcompile_server=false \
+    -Dprebuilt_server="${PREBUILT_SCRCPY_SERVER_PATH}" \
+    -Dc_args="-I$PWD/$DEPS_DIR/libusb-install/include" \
+    --wipe
+
+# Build scrcpy
+ninja -C "$BUILD_DIR"
+
+# Create distributable package
+mkdir -p "$DIST_DIR"
+cp "$BUILD_DIR/app/scrcpy" "$DIST_DIR/scrcpy-linux-v${VERSION}"
+cp "${PREBUILT_SCRCPY_SERVER_PATH}" "$DIST_DIR/"
+
+echo "Build complete! See $DIST_DIR"
+ls -la "$DIST_DIR"
