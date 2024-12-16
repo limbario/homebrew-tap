dfd875df7b572c030bcd109760b85c5b7988e94a
Karl Baumgartner <karl@limbar.io>
2024-12-16 15:34:05 +0300

    gh: add formula and version publishin
    Signed-off-by: Karl Baumgartner <karl@limbar.io>

---
+++ Formula/lim.rb
@@ -0,0 +1,34 @@
+class Limbar < Formula
+  desc "Get remote Android instances for local development and testing"
+  homepage "https://limbar.io"
+  version "v0.1.0"
+  license "Proprietary"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-darwin-arm64.tar.gz"
+      sha256 "replace_with_darwin_arm64_sha256" # replace_with_darwin_arm64_sha256
+    else
+      url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-darwin-amd64.tar.gz"
+      sha256 "replace_with_darwin_amd64_sha256" # replace_with_darwin_amd64_sha256
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-linux-arm64.tar.gz"
+      sha256 "replace_with_linux_arm64_sha256" # replace_with_linux_arm64_sha256
+    else
+      url "https://github.com/limbario/homebrew-lim/releases/download/v#{version}/lim-#{version}-linux-amd64.tar.gz"
+      sha256 "replace_with_linux_amd64_sha256" # replace_with_linux_amd64_sha256
+    end
+  end
+
+  def install
+    bin.install "lim"
+  end
+
+  test do
+    system "#{bin}/lim", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Limbario Lim
+
+## How do I install these formulae?
+
+`brew install limbario/lim/<formula>`
+
+Or `brew tap limbario/lim` and then `brew install <formula>`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "limbario/lim"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
