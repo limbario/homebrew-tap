b1159de5e182b6f1c17b028cebe33cca7110731a
Karl Baumgartner <karl@limbar.io>
2024-12-16 15:34:08 +0300

    scrcpy: build linux-amd64 and darwin-arm64 static libraries and commit them to the repo
    Signed-off-by: Karl Baumgartner <karl@limbar.io>

--- Formula/lim.rb
+++ Formula/lim.rb
@@ -5,8 +5,6 @@ class Lim < Formula
   license "Proprietary"
 
   depends_on "limbario/tap/adb"
-  # depends_on "limbario/tap/scrcpy"
-  depends_on "scrcpy"
 
   def caveats
     <<~EOS
@@ -17,9 +15,13 @@ class Lim < Formula
 
   on_macos do
     if Hardware::CPU.arm?
+      depends_on "limbario/tap/scrcpy"
+
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-darwin-arm64"
       sha256 "a9b5637bb20999bb580347ff12f3762a1f05b669505241ac4f179abd5078c15d" # replace_with_darwin_arm64_sha256
     else
+      depends_on "scrcpy"
+
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-darwin-amd64"
       sha256 "16bb4f057e5a474293f464e1f961abb76e5550ec43007f04ac457c16000ad3d1" # replace_with_darwin_amd64_sha256
     end
@@ -27,9 +29,13 @@ class Lim < Formula
 
   on_linux do
     if Hardware::CPU.arm?
+      depends_on "scrcpy"
+
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-linux-arm64"
       sha256 "8b84e8dd27bf08db32f5f3eacacf1dc109138845a7d8674f226ae642668bb85a" # replace_with_linux_arm64_sha256
     else
+      depends_on "limbario/tap/scrcpy"
+
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-linux-amd64"
       sha256 "0f21cb13dc7cfc7423ad706e39fcd10e8baba0eed249bc2f59c3605e0b1b3e18" # replace_with_linux_amd64_sha256
     end
