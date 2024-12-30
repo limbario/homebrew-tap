c9b73455865d148fe1fa8ff90e6f13d471f7837f
karlbaumg <karlbaumg@users.noreply.github.com>
2024-12-30 22:17:22 +0000

    Update to v0.8.10

--- Formula/lim.rb
+++ Formula/lim.rb
@@ -1,7 +1,7 @@
 class Lim < Formula
   desc "Get remote Android instances for local development and testing"
   homepage "https://limbar.io"
-  version "v0.8.9"
+  version "v0.8.10"
   license "Proprietary"
 
   depends_on "limbario/tap/adb"
@@ -18,13 +18,13 @@ class Lim < Formula
       depends_on "limbario/tap/scrcpy"
 
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-darwin-arm64"
-      sha256 "6070ea77edbcf36c159a1b4f3047c4fe389cebbac285d9b9d45321b3e087c79e" # replace_with_darwin_arm64_sha256
+      sha256 "dfa3a9edbdc6b1b37ff463a238d21201bf89784e98821bd3c1d9a312c8f49eed" # replace_with_darwin_arm64_sha256
     else
       # scrcpy does not yet publish static builds for darwin-amd64
       depends_on "scrcpy"
 
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-darwin-amd64"
-      sha256 "7846e1d585aae3040b5d1c608e8ba3a8ac7f15b21fab5e26c7bfdd7be16becfc" # replace_with_darwin_amd64_sha256
+      sha256 "3149ad76c98cd68b6f0ce33f1c4aefd9e9bd1b3abe0fe467fc1d9b3c1e7da764" # replace_with_darwin_amd64_sha256
     end
   end
 
@@ -34,12 +34,12 @@ class Lim < Formula
       depends_on "scrcpy"
 
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-linux-arm64"
-      sha256 "7a2643abe87f2a9bb0d3f5b4407ad93fe7a80d3358c89078efac6b5b229225a7" # replace_with_linux_arm64_sha256
+      sha256 "b1b6d0d59bc5f443d3c8b94574a063f961104aa9522d38ac02b2fff9d1a0afae" # replace_with_linux_arm64_sha256
     else
       depends_on "limbario/tap/scrcpy"
 
       url "https://github.com/limbario/homebrew-tap/releases/download/#{version}/lim-linux-amd64"
-      sha256 "68f2189a5821318c78727d359b0084b9755e4141c7635fd13237202c6bd5f930" # replace_with_linux_amd64_sha256
+      sha256 "e39ce74073e40d82d12cc866408bb30318bb9bfa5427f45e43490df7725d2cb4" # replace_with_linux_amd64_sha256
     end
   end
 
